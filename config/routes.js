36767c91931c18bcb937685efdeaaf0f02e64356
santosh suryawanshi <santosh.suryawanshi28@gmail.com>
2017-10-05 00:18:34 +0530

    Update secret keys

--- config/routes.js
+++ config/routes.js
@@ -9,14 +9,6 @@ var config = {
         googleAuth : '/auth/google',
         googleAuthCallback:'/auth/google/callback'
     },
-    host: 'http://localhost:3000',
-    facebook: {
-        appID: '299478470529543',
-        appSecret: 'ae7c0a440335a15cbdcfb7eeae2fbd36',
-    },
-    google:{
-        clientID:'118353661828-mcsrp5g2ksmub7ajvt1f46l7gao30oe1.apps.googleusercontent.com',
-        clientSecret :'T3g3IBKCT5xFkSPkpJw3okzn'
-    }
+    host: 'http://localhost:3000'
 };
 module.exports = config;
